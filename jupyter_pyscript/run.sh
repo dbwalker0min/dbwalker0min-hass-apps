edb86b2a46f20e23e6b2425c11fd445f5f6d6e2d
David Walker <dbwalker0min@gmail.com>
2026-02-21 19:04:00 -0800

    Right endpoint

--- jupyter_pyscript/run.sh
+++ jupyter_pyscript/run.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 echo "Loading options from /data/options.json..."
 echo $SUPERVISOR_TOKEN
 echo `whoami`
-curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/addons/self/config
+curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/addons/self/options/config
